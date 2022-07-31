809a00e63ead88b8e8e5188197f7e78b3f6980c9
Mariusz <mariusz.janowski@yahoo.pl>
2022-07-31 21:12:41 +0200

    alexMistakes in kata

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
-function getDivisorsCnt(n) {
-  // todo
-  console.log("Manio");
-}
+const alexMistakesTime = {
+  1: 5,
+  2: 10,
+  3: 20,
+  4: 40,
+  5: 80,
+};
 
-getDivisorsCnt();
+const alexMistakesTimeArr = [5, 10, 20, 40, 80];
+
+const oneKata = 60 / 10;
+
+function alexMistakes(numberOfKata, timeLimit) {
+  //TODO
+  const katasTime = numberOfKata * oneKata;
+  const mistakeTime = timeLimit - katasTime;
+
+  let sum = 0;
+
+  //pickup one result from array and divide it by index number
+
+  alexMistakesTimeArr.forEach((el, i) => {
+    sum += alexMistakesTime[i + 1];
+    if (sum > mistakeTime) {
+      console.log(sum);
+    }
+  });
+}
+alexMistakes(10, 120);
+/*
+
+describe("Tests", () => {
+  it("test", () => {
+Test.assertEquals(alexMistakes(10, 120), 3);
+Test.assertEquals(alexMistakes(11, 120), 3);
+Test.assertEquals(alexMistakes(3, 45), 2);
+Test.assertEquals(alexMistakes(8, 120), 3);
+Test.assertEquals(alexMistakes(6, 60), 2);
+Test.assertEquals(alexMistakes(9, 180), 4);
+  });
+});
+
+  
+  */
