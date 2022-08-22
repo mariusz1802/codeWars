7bc220903235c165fe6b0b0e238577ceae6460a1
Mariusz <mariusz.janowski@yahoo.pl>
2022-08-22 13:57:43 +0200

    refactoring code

--- script.js
+++ script.js
@@ -1,3 +1,49 @@
+// let url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=the%20weekend&key=AIzaSyBBUzPFtZAiqnp-4RdAiFfHjBzOWMWJhlE&maxResults=25`;
+
+// const getData = async function (url) {
+//   const response = await fetch(url);
+//   let data = await response.json();
+//   return data;
+// };
+
+// const videoId = async function (url) {
+//   const responseId = await getData(url);
+//   const VideoId = await responseId.items;
+//   VideoId.forEach((element) => {
+//     const idEl = element.id.videoId;
+//     const newEl = `https://www.youtube.com/watch?v=${idEl}`;
+//     return newEl;
+//   });
+// };
+
+// const videoTitle = async function (url) {
+//   const responseTitle = await getData(url);
+//   const Title = await responseTitle.items;
+//   Title.forEach((element) => {
+//     const idEl = element.snippet.title;
+//     return idEl;
+//   });
+// };
+
+// const thumbnails = async function (url) {
+//   const responseThumbnail = await getData(url);
+//   const Thumbnail = await responseThumbnail.items;
+
+//   Thumbnail.forEach((element) => {
+//     const thumbnail = element.snippet.thumbnails.high.url;
+//     return thumbnail;
+//   });
+// };
+
+// const button = document.querySelector('button');
+
+// button.addEventListener('click', () => {
+//   videoId(url);
+//   videoTitle(url);
+//   thumbnails(url);
+
+// });
+
 let url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=the%20weekend&key=AIzaSyBBUzPFtZAiqnp-4RdAiFfHjBzOWMWJhlE&maxResults=25`;
 
 const getData = async function (url) {
@@ -6,45 +52,63 @@ const getData = async function (url) {
   return data;
 };
 
-const videoId = async function (url) {
-  const responseId = await getData(url);
-  const VideoId = await responseId.items;
-  VideoId.forEach((element) => {
-    const idEl = element.id.videoId;
-    const newEl = `https://www.youtube.com/watch?v=${idEl}`;
-    return newEl;
-  });
-};
-
-const videoTitle = async function (url) {
-  const responseTitle = await getData(url);
-  const Title = await responseTitle.items;
-  Title.forEach((element) => {
-    const idEl = element.snippet.title;
-    return idEl;
-  });
-};
-
-const thumbnails = async function (url) {
-  const responseThumbnail = await getData(url);
-  const Thumbnail = await responseThumbnail.items;
-
-  Thumbnail.forEach((element) => {
-    const thumbnail = element.snippet.thumbnails.high.url;
-    return thumbnail;
-  });
-};
-
-// async function getUrl() {
-//   const returnUrl = getUrl.then((el) => {
-//     console.log(el.items);
+// const videoId = async function (url) {
+//   const responseId = await getData(url);
+//   const VideoId = await responseId.items;
+//   VideoId.forEach((element) => {
+//     const idEl = element.id.videoId;
+//     const newEl = `https://www.youtube.com/watch?v=${idEl}`;
+//     return newEl;
 //   });
-//   return returnUrl;
-// }
+// };
 
-const button = document.querySelector("button");
+const videoIdAble = (state) => ({
+  videoId: function () {
+    return console.log(`id video to: ${state.videoId}`);
+  },
+});
 
-button.addEventListener("click", () => {
+const titleAble = (state) => ({
+  title: function () {
+    return console.log(`title video : ${state.title}`);
+  },
+});
+const thumbnailAble = (state) => ({
+  thumbnail: function () {
+    return console.log(`thumbnail : ${state.thumbnail}`);
+  },
+});
+
+const searchResult = async (name, videoId, title, thumbnail) => {
+  const Data = await getData(url);
+  const Items = await Data.items; //Array of Objects
+
+  Items.forEach((element) => {
+    console.log(element);
+  });
+
+  const state = {
+    name: name,
+    videoId: videoId,
+    title: title,
+    thumbnail: thumbnail,
+  };
+
+  return Object.assign(
+    {},
+    videoIdAble(state),
+    titleAble(state),
+    thumbnailAble(state)
+  );
+};
+
+const newVideo = searchResult('MojeVideo', '1234', 'Blade', 'picturethumbnail');
+
+newVideo.videoId();
+
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
   videoId(url);
   videoTitle(url);
   thumbnails(url);
