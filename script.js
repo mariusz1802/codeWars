091b36adedd5f9e4868649afb36669b4a3bef802
Mariusz <mariusz.janowski@yahoo.pl>
2022-09-04 19:32:51 +0200

    YouTube search without suggestions

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const input = document.getElementById("search");
+
 //function to get input value
 function getInputValue() {
   const value = document.getElementById("search").value;
@@ -15,15 +17,28 @@ class DivCreator {
     this.videoId = videoId;
   }
   createDiv() {
+    //Create a href
+    const a = document.createElement("a");
+    a.href = `https://www.youtube.com/watch?v=${this.videoId}`;
+    //Create Div container
     const el = document.createElement("div");
     el.classList.add("search-box");
+    //Title
     const title = document.createElement("h2");
     title.innerHTML = this.title;
+    //Description
     const description = document.createElement("p");
     description.innerHTML = this.description;
+    //Small Img(Thumbnail)
     const thumbnail = document.createElement("img");
+    thumbnail.classList.add("img");
     thumbnail.src = this.thumbnail;
     thumbnail.alt = this.title;
+    //Chanel Title
+
+    //Add elements to body
+    a.appendChild(el);
+    document.body.appendChild(a);
     el.appendChild(title);
     el.appendChild(thumbnail);
     el.appendChild(description);
@@ -31,8 +46,24 @@ class DivCreator {
   }
 }
 
+class TitleCreator {
+  constructor(title) {
+    this.title = title;
+  }
+  showTitle() {
+    const el = document.createElement("div");
+    el.classList.add("search-box");
+    const title = document.createElement("h2");
+    title.innerHTML = this.title;
+    el.appendChild(title);
+  }
+}
+
+const newTitle = new TitleCreator("Manio");
+newTitle.showTitle();
+
 function showData() {
-  const showData = clickButton();
+  const showData = neededData();
   showData.then((element) => {
     element.forEach((el) => {
       const newEl = new DivCreator(
@@ -42,12 +73,12 @@ function showData() {
         el.chanelTitle,
         el.videoId
       );
-      return newEl;
+      return newEl.createDiv();
     });
   });
 }
 
-const clickButton = function () {
+const neededData = function () {
   const query = getInputValue();
   let url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=AIzaSyBBUzPFtZAiqnp-4RdAiFfHjBzOWMWJhlE&maxResults=25`;
   const selectedData = returnSelectedData(url);
@@ -80,3 +111,9 @@ const returnSelectedData = async function (url) {
 };
 
 button.addEventListener("click", showData);
+input.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    showData();
+  }
+});
