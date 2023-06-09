e136cac7f5753534ddfaea6ca83e4a066fb09852
BombaDesign <mariusz.janowski@yahoo.pl>
2023-06-09 09:42:09 +0200

    find higher number(binary representation)

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-function toCamelCase(str) {
-  const replaceStr = str.replace(/-/g, "");
-  console.log(replaceStr);
+function nextHigher(n) {
+  const binaryRepresentation = n.toString(2);
+  const howManyBits = bitsCounter(binaryRepresentation);
+  nextHigherNumber(n, howManyBits);
+
+  do {
+    const higherNumber = n++;
+    const higherBinaryRepresentation = higherNumber.toString(2);
+    const howManyBitsHigherNumber = bitsCounter(higherBinaryRepresentation);
+  } while (howManyBits === howManyBitsHigherNumber);
 }
 
-toCamelCase("the-stealth-warrior");
+function bitsCounter(x) {
+  const character = "1";
+
+  let regex = new RegExp(character, "g");
+  let matches = x.match(regex);
+
+  const count = matches ? matches.length : 0;
+  return count;
+}
+
+function nextHigherNumber(baseNumber, bitsNumber) {
+  const higherNumber = baseNumber++;
+  const higherBinaryRepresentation = higherNumber.toString(2);
+  const howManyBitsHigherNumber = bitsCounter(higherBinaryRepresentation);
+
+  for (i = baseNumber; bitsNumber === howManyBitsHigherNumber; i++) {}
+}
+
+nextHigher(10);
