c92f52d7434d8150fb792c9b6512a1077f9297bd
BombaDesign <mariusz.janowski@yahoo.pl>
2023-06-11 13:44:22 +0200

    find next higher improvements

--- script.js
+++ script.js
@@ -1,31 +1,29 @@
 function nextHigher(n) {
+  const BitsOne = howManyBits(n);
+  const BitsTwo = findNextNumber(n);
+  return { bitsOne: BitsOne, bistTwo: BitsTwo };
+}
+
+function findNextNumber(n) {
+  const nextNumber = ++n;
+  const binaryRepresentation = nextNumber.toString(2);
+  const howManyBits = bitsCounter(binaryRepresentation);
+  const fun = nextHigher(n);
+  console.log(fun.bitsOne);
+}
+
+function howManyBits(n) {
   const binaryRepresentation = n.toString(2);
   const howManyBits = bitsCounter(binaryRepresentation);
-  nextHigherNumber(n, howManyBits);
-
-  do {
-    const higherNumber = n++;
-    const higherBinaryRepresentation = higherNumber.toString(2);
-    const howManyBitsHigherNumber = bitsCounter(higherBinaryRepresentation);
-  } while (howManyBits === howManyBitsHigherNumber);
+  return howManyBits;
 }
 
 function bitsCounter(x) {
   const character = "1";
-
   let regex = new RegExp(character, "g");
   let matches = x.match(regex);
-
   const count = matches ? matches.length : 0;
   return count;
-}
-
-function nextHigherNumber(baseNumber, bitsNumber) {
-  const higherNumber = baseNumber++;
-  const higherBinaryRepresentation = higherNumber.toString(2);
-  const howManyBitsHigherNumber = bitsCounter(higherBinaryRepresentation);
-
-  for (i = baseNumber; bitsNumber === howManyBitsHigherNumber; i++) {}
 }
 
 nextHigher(10);
